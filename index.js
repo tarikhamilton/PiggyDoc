f0c3d4d98ba62d716e52397792c4463ce3abe10a
Tarik Hamilton <tarikhamilton@gmail.com>
2016-02-12 01:17:47 -0500

    Basic functionality: read each line and parse

--- index.js
+++ index.js
@@ -1,21 +1,92 @@
 var fs = require('fs')
 
+var shortcode = {
+    name: '',
+    attributes: []
+}
 
 var generate = function() {
     fs.readFile('wp-shortcode-documentor/test.php', 'utf8', function(error, data) {
-        console.log('start')
+        /**
+         * Array of your pigs
+         */
+        var pigs = []
+        
         if (!error) {
-            console.log(data)
             var regex = /(\/\*{2})([\s\S]+?)\*\//g,
-                docblocks = data.match(regex)
+                docblocks = data.match(regex)            
+            /*
+             * pig, as in piggyback. This is supposed to go in a regular docblock.
+             * 
+             * pig.trigger identifies docblocks with your custom tag and scan the rest
+             *             the custom parameters.
+             * 
+             * pig.tags an array that houses the custom parameters.
+             * 
+             * IMPORTANT!!!
+             * You must escape all characters that would be escaped in Regex.
+             * 
+             * When using new RegExp (allows for a variable to go into the regex)
+             * you must escape for the JavaScript String, and again for regex.
+             * 
+             * Ex: \\$param
+             * 
+             */
+           
+            var param = {
+                name: '\\$param'
+            }
             
-            console.log(docblocks)
+            var pig = {
+                trigger: '\@shortcode', 
+                tags: [param]
+            }
             
             docblocks.forEach(function(docblock, i) {
                 if (docblock.match(/\@shortcode/g)) {
-                //if (docblock.includes('@shortcode')) {
-                    console.log('Docblock ' + i + ' includes a shortcode')
-               }      
+                    var lines = docblock.split('\n')
+                    
+                    // at each line, check if any of the parameters are present at the start
+                    lines.forEach(function(line, i) {
+                        pig.tags.forEach(function(tag) {                        
+                            var tagRegex = new RegExp('\\s\\*\\s' + tag.name),
+                                match = line.search(tagRegex) // How does Harry like this when VCS doesn't highlight variables on subsequent lines?!
+                            
+                            if (match != -1) {             
+                                /*
+                                 * For some reason line.split(' ') returns
+                                 * 
+                                 * [ '',
+                                 *   '*',
+                                 *   '$param',
+                                 *  ...
+                                 * ]
+                                 * 
+                                 * There's an empty string in the beginning... 
+                                 * Rather unpredictable. Let's remove that later.
+                                 * 
+                                 * This effects:
+                                 *   var name,
+                                 *   split.splice
+                                 */                                               
+                                var split = line.split(' ')
+                                
+                                // Get the tag's name
+                                var name = split[2]
+                                
+                                // Get the description
+                                split.splice(0,3) // 
+                                split = split.join(' ')
+                                var desc = split
+                                console.log(desc)
+                                
+                            }
+                        })
+                        
+                        
+                    })
+                    
+                }      
             })          
         } else {
             console.log(error)           
---
+++ test.php
@@ -0,0 +1,26 @@
+pre php
+<?php
+stuff
+/**
+ * Shortcode A
+ *
+ * @shortcode cta
+ * $param link|required The destination for your link
+ * $param style The appearance
+ */
+ 
+/**
+ *
+ * Shortcode B
+ *
+ * @shortcode
+ */
+ 
+/**
+ * Random Docblock
+ *
+ */
+ 
+ sup
+ ?>
+ test
